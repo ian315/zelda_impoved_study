0ed210ab55fb38a28029ec6ee8063b2970015186
ian315 <ianpratis9@gmail.com>
2025-04-23 18:31:42 -0500

    criando classe entidade, atualizando classe spritesheet e criando classe player

---
+++ src/com/ian/entities/Player.java
@@ -0,0 +1,9 @@
+package com.ian.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Player extends Entity{
+    public Player(int x, int y, int width, int height, BufferedImage sprite) {
+        super(x, y, width, height, sprite);
+    }
+}
--- src/com/ian/graphics/Spritesheet.java
+++ src/com/ian/graphics/Spritesheet.java
@@ -1,4 +1,23 @@
 package com.ian.graphics;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Spritesheet {
+
+    BufferedImage spritesheet;
+
+    public Spritesheet(String path) {
+        try {
+            spritesheet = ImageIO.read(Objects.requireNonNull(getClass().getResource(path)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public BufferedImage getSprite(int x, int y, int widht, int height) {
+        return spritesheet.getSubimage(x, y, widht, height);
+    }
 }
